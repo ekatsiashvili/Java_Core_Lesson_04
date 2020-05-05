b0d3b733d34ebb45c1100708e330527f5dcaae8e
Elena <katsiashvili.elena@gmail.com>
2020-05-05 22:35:51 +0300

    Change package for from ua.lviv.lgs to part_1 and add package part_2

---
+++ src/part_2/Animal.java
@@ -0,0 +1,54 @@
+package ua.lviv.lgs;
+
+public class Animal {
+
+	private String name;
+	private double speed;
+	private int age;	
+	
+	
+	public Animal(String name, double speed, int age) {
+		super();
+		this.name = name;
+		this.speed = speed;
+		this.age = age;
+	}
+
+
+	String getName() {
+		return name;
+	}
+
+
+	void setName(String name) {
+		this.name = name;
+	}
+
+
+	double getSpeed() {
+		return speed;
+	}
+
+
+	void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+
+	int getAge() {
+		return age;
+	}
+
+
+	void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+
+	
+	
+	
+	
+}
---
+++ src/part_2/App.java
@@ -0,0 +1,16 @@
+package ua.lviv.lgs;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Animal a = new Animal("Dragon", 300.98, 200);			
+		System.out.println("Animal name = "+a.getName()+", Animal speed = "+a.getSpeed()+" km/h"+", Animal age = "+a.getAge()+" years"+"\n-------------------------------------------------------------------------------");
+		
+		a.setName("Lion"); a.setSpeed(100.70); a.setAge(30);		
+		System.out.println("\n"+"Animal name = "+a.getName()+", Animal speed = "+a.getSpeed()+" km/h"+", Animal age = "+a.getAge()+" years");
+		
+
+	}
+
+}
